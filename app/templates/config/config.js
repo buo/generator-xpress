95384bbb1bfc33788474e81c9514be8a26e64e6b
buo <buo@users.noreply.github.com>
2015-08-03 13:32:56 +0900

    Support OpenShift

--- app/templates/config/config.js
+++ app/templates/config/config.js
@@ -21,13 +21,14 @@ var config = {
     db: 'mongodb://localhost/<%= slugify(appname) %>-test'
   },
 
-  production: {
+  openshift: {
     root: rootPath,
     app: {
       name: '<%= slugify(appname) %>'
     },
-    port: 3000,
-    db: 'mongodb://localhost/<%= slugify(appname) %>'
+    ipaddress: process.env.OPENSHIFT_NODEJS_IP,
+    port: process.env.OPENSHIFT_NODEJS_PORT,
+    db: process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_GEAR_NAME
   }
 };
 
